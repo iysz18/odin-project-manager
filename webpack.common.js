4d83bcb561c2652a44e98c232ced7a7c22604593
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-07-09 16:51:41 +0200

    build(webpack module, plugins): add module and template src.

--- webpack.common.js
+++ webpack.common.js
@@ -7,9 +7,18 @@
    },
    plugins: [
      new HtmlWebpackPlugin({
+       template: "./src/template.html",
        title: 'Production',
      }),
    ],
+   module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }
+    ],
+   },
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
